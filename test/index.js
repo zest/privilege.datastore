0252ed9522686b96a6fa111179d4636595a8c50c
pranav-jha <jha.pranav.s@gmail.com>
2014-09-02 15:43:33 +0530

    migrated to mocha and chai
    migrated to mocha and chai

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 'use strict';
-var datasource = require('./injector')();
-describe('privilege.datasource', function () {
+var datastore = require('../lib'),
+    expect = require('chai').expect;
+describe('privilege.datastore', function () {
     // it should return a module
     it('it should return a module', function () {
-        expect(datasource).not.toBe(undefined);
+        expect(datastore).not.to.equal(undefined);
     });
 });
